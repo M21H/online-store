d0db74eea4af93f6fa3a90196e7259f23c744509
M21H <maxym.hyba@gmail>
2021-06-15 13:38:45 +0300

    added registration; authorization;  jwt token; bcrypt

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -2,7 +2,7 @@ const Router = require('express')
 const router = new Router()
 const userContriller = require('../controllers/userContriller')
 
-router.post('/reginstration', userContriller.registration)
+router.post('/registration', userContriller.registration)
 router.post('/login', userContriller.login)
 router.get('/auth', userContriller.check)
 
